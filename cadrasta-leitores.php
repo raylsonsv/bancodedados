2818cd408a07d0f74a9a310f5a5de7efd6710c75
raylsonsv <168122263+raylsonsv@users.noreply.github.com>
2024-10-08 18:40:47 -0300

    Add files via upload

--- cadrasta-leitores.php
+++ cadrasta-leitores.php
@@ -1,4 +1,4 @@
-  <?php
+ <?php
  // inclui o arquivo de configuração (conexão com banco de dados)
  include"config.php";
  // Verifica se a conexão com o banco de dados foi bem-sucedida
@@ -9,26 +9,36 @@
 
 
  //receba os dados do formulário
- $nome = '';
- $datanasc = '';
- $celular = '';
- $email = '';
- $ra = '';
- $endereco = '';
- $num_end = '';
- $bairro = '';
- $cidade = '';
+ $nome = 'Teste Nome';
+ $dtnasc = '2001-10-20';
+ $celular = '(81) 99999-9999';
+ $email = 'teste@teste.com';
+ $ra = '1111111';
+ $endereco = 'Rua Teste';
+ $num_end = '99';
+ $bairro = 'Janga';
+ $cidade = 'Paulista/PE';
 
  // criar variáveis para inserir o resistro
  $sql ="INSERT INTO `leitores`
- (, `Nome`, `DtNasc`, `Celular`, `Email`, `RA`, `Endereco`, `NumEnd`, `Bairro`, 
+ (`Nome`, `DtNasc`, `Celular`, `Email`, `RA`, `Endereco`, `NumEnd`, `Bairro`, 
  `CidadeUF`) VALUES 
 ('$nome','$dtnasc','$celular','$email','$ra','$endereco','$num_end','$bairro',
 '$cidade')";
 
 // Executa a consulta SQL. falhar, exibe o erro do banco de dados
 $query = mysqli_query(mysql: $conn,query: $sql) or
-die(mysqli_error(mysql: $db));
+die(mysqli_error(mysql: $conn));
 
- 
-  ?>
+if($query){
+     echo "<center>";
+     echo "Cadrasto realizado com sucesso!! <br>";
+     echo "<a href='index.php'><button title='Home pago'>Voltar<button></a>";
+     echo "</center>";
+} else {
+  echo "<center>";
+  echo "<a href='index.php'><button title='Home pago'>Voltar<button></a>";
+  echo "</center>";
+}
+  ?>      
+  
