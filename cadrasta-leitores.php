a5024f99b0356ed42d1c561962b86768cf45065a
raylsonsv <168122263+raylsonsv@users.noreply.github.com>
2024-10-08 14:50:10 -0300

    Update cadrasta-leitores.php

--- cadrasta-leitores.php
+++ cadrasta-leitores.php
@@ -1,10 +1,15 @@
- <?php
+  <?php
+ // inclui o arquivo de configuração (conexão com banco de dados)
  include"config.php";
+ // Verifica se a conexão com o banco de dados foi bem-sucedida
  if (!$conn){
-    die("Falha na conexão{mysqli_connect_error()}");
+   // Se a conexão falhar, exibe uma mensagem de erro e encerra a execução
+    die("Falha na conexão: " . mysqli_connect_error());
  }
+
+
  //receba os dados do formulário
- $nome ='';
+ $nome = '';
  $datanasc = '';
  $celular = '';
  $email = '';
@@ -14,7 +19,16 @@
  $bairro = '';
  $cidade = '';
 
- //criar variáveis para inserir o resistro
- $aql ="";
+ // criar variáveis para inserir o resistro
+ $sql ="INSERT INTO `leitores`
+ (, `Nome`, `DtNasc`, `Celular`, `Email`, `RA`, `Endereco`, `NumEnd`, `Bairro`, 
+ `CidadeUF`) VALUES 
+('$nome','$dtnasc','$celular','$email','$ra','$endereco','$num_end','$bairro',
+'$cidade')";
+
+// Executa a consulta SQL. falhar, exibe o erro do banco de dados
+$query = mysqli_query(mysql: $conn,query: $sql) or
+die(mysqli_error(mysql: $db));
+
  
   ?>
